8f57e5473f9baff79c8423de6250bc455285b6a9
颢珑 张 <edwardazazel@126.com>
2018-04-16 23:36:05 -0400

    Project done

---
+++ WebApplication1/Controllers/UsersController.cs
@@ -0,0 +1,169 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class UsersController : Controller
+    {
+        private UserContext db = new UserContext();
+        public static int UserCount = 0;
+
+        // GET: Users
+        public ActionResult Index()
+        {
+            //the following to lines remove all rows, this line should not be executed for !!ANY reasons!!
+            //db.Users.RemoveRange(db.Users);
+            //db.SaveChanges();
+
+            System.Diagnostics.Debug.WriteLine(db.Users.Count());
+            
+
+            //insert data from .json file
+            if (db.Users.Count<User>() == 0)
+            {
+                System.Diagnostics.Debug.WriteLine("we are here");
+
+                var outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase);
+                var iconPath = Path.Combine(outPutDirectory, "..\\App_Data\\MOCK_DATA.json");
+                string icon_path = new Uri(iconPath).LocalPath;
+                //C:\Program Files (x86)\App_Data\MOCK_DATA.json
+                using (StreamReader r = new StreamReader(icon_path))
+                {
+                    string json = r.ReadToEnd();
+                    List<UserData> UserDatas = JsonConvert.DeserializeObject<List<UserData>>(json);
+
+                    foreach (UserData temp in UserDatas)
+                    {
+                        db.Users.Add(new User
+                        {
+                            UserID = ++UserCount,
+                            LastName = temp.LastName,
+                            FirstName= temp.FirstName,
+                            Email = temp.Email
+                        });
+                        
+                    }
+                    db.SaveChanges();
+                }
+
+
+            }
+
+            return View(db.Users.ToList());
+        }
+
+        // GET: Users/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UserID,LastName,FirstName,Email")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(user);
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UserID,LastName,FirstName,Email")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            User user = db.Users.Find(id);
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication1/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+namespace WebApplication1.Models
+{
+    public class User : UserData
+    {
+        public int UserID { get; set; }
+        
+    }
+
+    public class UserData
+    {
+        [JsonProperty(PropertyName = "last_name")]
+        public string LastName { get; set; }
+        [JsonProperty(PropertyName = "first_name")]
+        public string FirstName { get; set; }
+        [JsonProperty(PropertyName = "email")]
+        public string Email { get; set; }
+    }
+
+    public class UserContext : DbContext {
+        public DbSet<User> Users { get; set; }
+    }
+}
